d1282cfae47f71d308758017073fe0df70416cfc
Zo <960gd7@gmail.com>
2020-03-13 16:21:04 +0300

    Updates

---
+++ precache-manifest.efc3461540136613f9c2ba2c0f1bd358.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c1040f8d146c0323350629813005ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "96a19911b0ea809040ba",
+    "url": "/monsters-rolodex/static/css/main.aa470901.chunk.css"
+  },
+  {
+    "revision": "df9992fde5d0af220241",
+    "url": "/monsters-rolodex/static/js/2.8c403217.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.8c403217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a19911b0ea809040ba",
+    "url": "/monsters-rolodex/static/js/main.ca8f55b8.chunk.js"
+  },
+  {
+    "revision": "00653b72cb2ab9f492e4",
+    "url": "/monsters-rolodex/static/js/runtime-main.4d62735d.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/monsters-rolodex/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
